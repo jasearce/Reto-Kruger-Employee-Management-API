467c854f2e3e93ff70f22f543f079087990b65e7
Javier <jasearce@espol.edu.ec>
2022-11-29 23:33:32 -0500

    Initial Commit

---
+++ src/main/java/com/jasearce/krugeremployeemanagement/KrugerEmployeeManagementApplication.java
@@ -0,0 +1,13 @@
+package com.jasearce.krugeremployeemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KrugerEmployeeManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KrugerEmployeeManagementApplication.class, args);
+	}
+
+}
