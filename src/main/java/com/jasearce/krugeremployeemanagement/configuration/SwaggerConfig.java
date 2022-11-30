ceb9e413ccd3eaaf76f75d930d381565f60e236a
Javier <jasearce@espol.edu.ec>
2022-11-30 17:55:52 -0500

    Change configuration of Swagger

---
+++ src/main/java/com/jasearce/krugeremployeemanagement/configuration/SwaggerConfig.java
@@ -0,0 +1,34 @@
+package com.jasearce.krugeremployeemanagement.configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+	
+	public Docket postAPI() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.groupName("public-api")
+				.apiInfo(apiInfo())
+				.select()
+				.build();
+	}
+
+	private ApiInfo apiInfo() {
+		// TODO Auto-generated method stub
+		return new ApiInfoBuilder()
+				.title("Kruger Employees API with SpringBoot)")
+				.description("Kruger Employees API reference for developers")
+				.termsOfServiceUrl("https://github.com/jasearce/Reto-Kruger-Employee-Management-API")
+				.license("Apache License Version 2.0")
+				.licenseUrl("https://github.com/springfox/springfox/blob/master/LICENSE")
+				.version("3.0")
+				.build();
+	}
+}
