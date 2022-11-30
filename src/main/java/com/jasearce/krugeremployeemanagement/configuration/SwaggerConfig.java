097c1dd4a73970f03d3887d5964ed221072b264b
Javier <jasearce@espol.edu.ec>
2022-11-30 18:04:04 -0500

    Modificacion de la documentacion de la API con configuracion de Swagger

--- src/main/java/com/jasearce/krugeremployeemanagement/configuration/SwaggerConfig.java
+++ src/main/java/com/jasearce/krugeremployeemanagement/configuration/SwaggerConfig.java
@@ -1,9 +1,11 @@
 package com.jasearce.krugeremployeemanagement.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -12,9 +14,9 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfig {
 	
+	@Bean
 	public Docket postAPI() {
 		return new Docket(DocumentationType.SWAGGER_2)
-				.groupName("public-api")
 				.apiInfo(apiInfo())
 				.select()
 				.build();
@@ -23,12 +25,13 @@ public class SwaggerConfig {
 	private ApiInfo apiInfo() {
 		// TODO Auto-generated method stub
 		return new ApiInfoBuilder()
-				.title("Kruger Employees API with SpringBoot)")
+				.title("Kruger Employees API with SpringBoot")
 				.description("Kruger Employees API reference for developers")
 				.termsOfServiceUrl("https://github.com/jasearce/Reto-Kruger-Employee-Management-API")
 				.license("Apache License Version 2.0")
 				.licenseUrl("https://github.com/springfox/springfox/blob/master/LICENSE")
 				.version("3.0")
+				.contact(new Contact("Javier Arce", "https://github.com/jasearce/Reto-Kruger-Employee-Management-API", null))
 				.build();
 	}
 }
